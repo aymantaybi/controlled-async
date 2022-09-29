13b16ef9254b86e32e8144433ffaacbc0b0a17f8
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2022-09-29 22:39:15 +0100

    feat: add timeout option

--- exemple.js
+++ exemple.js
@@ -1,20 +1,18 @@
-const createControlledAsync = require('./index');
+const { createControlledAsync } = require("./index");
 
 (async () => {
+  let [controlledSleep, sleepController] = createControlledAsync(sleep);
 
-    let [controlledSleep, sleepController] = createControlledAsync(sleep);
+  setTimeout(() => {
+    sleepController.resolve({ done: false });
+  }, 2000);
 
-    setTimeout(() => {
-        sleepController.resolve({ done: false });
-    }, 2000);
-
-    let result = await controlledSleep(5000);
-    console.log(result);
-
+  let result = await controlledSleep(15000);
+  console.log(result);
 })();
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const Controller = require('./controller');
+const Controller = require("./controller");
 
-function createControlledAsync(asyncFunction) {
-    let controller = new Controller();
-    let controlledAsync = (...params) => new Promise((resolve, reject) => {
-        asyncFunction(...params).then(value => {
-            resolve(value);
-        });
-        controller.on('resolve', (value) => {
-            resolve(value);
-        });
-        controller.on('reject', (value) => {
-            resolve(value);
-        });
+function createControlledAsync(asyncFunction, options = { timeout: 0 }) {
+  let controller = new Controller();
+  let controlledAsync = (...params) =>
+    new Promise((resolve, reject) => {
+      var timeoutId = options?.timeout
+        ? setTimeout(() => {
+            resolve(false);
+          }, options.timeout)
+        : null;
+      asyncFunction(...params).then((value) => {
+        clearTimeout(timeoutId);
+        resolve(value);
+      });
+      controller.on("resolve", (value) => {
+        clearTimeout(timeoutId);
+        resolve(value);
+      });
+      controller.on("reject", (value) => {
+        clearTimeout(timeoutId);
+        resolve(value);
+      });
     });
-    return [controlledAsync, controller];
-};
+  return [controlledAsync, controller];
+}
 
-module.exports = createControlledAsync;
+module.exports = { createControlledAsync, Controller };
